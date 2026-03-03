19b4070504c68a9684f90fa487aa22f3ef6ad5c1
Kent Joemar L. Escoto <kentescoto24@gmail.com>
2026-03-03 20:59:40 +0800

    initialize with tailwindcss and lucide icons

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Camera } from 'lucide-vue-next';
+</script>
+
+<template>
+  <Camera color="red" :size="32" />
+
+  <p class="text-red-500 text-2xl font-bold">test tailwind</p>
+
+  <h1 class="text-3xl font-bold underline text-white">
+    Hello world!
+  </h1>
+</template>
